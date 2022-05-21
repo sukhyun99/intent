6967906700d5ac5a09fad80f583a2f0fa9b124ff
weekendHuman <buddah99@naver.com>
2022-05-21 14:16:00 +0900

    다른 액티비티 추가

--- app/src/main/java/com/example/intent/OtherActivity.kt
+++ app/src/main/java/com/example/intent/OtherActivity.kt
@@ -2,15 +2,10 @@ package com.example.intent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        btnMove.setOnClickListener {
-
-        }
+        setContentView(R.layout.activity_other)
     }
 }
